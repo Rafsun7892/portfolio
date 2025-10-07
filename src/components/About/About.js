51ded38eaad41bb0269caa0085834909793f9d4c
Rafsun Siddique <siddiquerafsun@gmail.com>
2025-10-07 16:20:47 +0600

    CV uploaded

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -35,7 +35,7 @@ const About = () => {
 
       <div className='about__contact center'>
         {resume && (
-          <a href={resume} download>
+          <a href={resume}>
             <span type='button'className='btn btn--outline'>
               Resume</span>
         </a>
